7b9051e1fe4ccd3d148d709c607ad594264e7080
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-08 02:30:37 +0530

    change in something

--- src/Container/NewsAndEvents/index.js
+++ src/Container/NewsAndEvents/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ErrorBoundary from '../../Components/ErrorBoundary';
+import NewsShowcase from '../../Components/NewsShowcase';
 import { Slideshow } from '../../Components/Slider/slideshow';
 import { BigPArtzBrands, BigTireBrands } from '../../data.js';
 const NewsAndEvents = () => {
   return (
     <>
-      <div className="mt-28">
-        <ErrorBoundary>
-          <Slideshow
-            data={BigPArtzBrands[0].Brands}
-            heading={'News & Events'}
-          ></Slideshow>
-        </ErrorBoundary>
+      <div className=" text-36px font-semibold text-Heading flex flex-col justify-center items-center">
+        <h2>News & Events</h2>
+        <div className="flex flex-row justify-center items-center m-6">
+          <NewsShowcase></NewsShowcase>
+          <NewsShowcase></NewsShowcase>
+          {/* <ErrorBoundary>
+            <Slideshow
+              data={BigPArtzBrands[0].Brands}
+              heading={null}
+              smallSlider={true}
+              singleSlider={true}
+            ></Slideshow>
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <Slideshow
+              data={BigPArtzBrands[0].Brands}
+              // smallSlider={true}
+              heading={null}
+              singleSlider={true}
+            ></Slideshow>
+          </ErrorBoundary> */}
+        </div>
       </div>
     </>
   );
