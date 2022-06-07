cd89174bc6fd8aed061b5b00ebf38613cabd20df
Kd <umarkhurshid3@gmail.com>
2022-06-08 03:49:09 +0530

    contactimage update

--- src/Container/ContactFormContainer/index.js
+++ src/Container/ContactFormContainer/index.js
@@ -3,7 +3,7 @@ import Contactform from '../../Components/ContactForm/';
 
 export const ContactFormContainer = () => {
   return (
-    // <div id="maincontainer" className='w-70vh' >    
+    // <div id="maincontainer" className='w-70vh' >
     <div
       className=" flex md:flex lg:w-screen w-fit    lg:flex-row flex-col align-center justify-center "
       id="ContactSection"
@@ -28,21 +28,19 @@ export const ContactFormContainer = () => {
           width="300"
         />
       </div> */}
-      <div className="flex-1 ">  
+      <div className="flex-1 ">
         <div
-          className={
-            'h-5/6 object-scale-down bg-no-repeat ' 
-          }
+          className={"h-5/6 object-scale-down bg-no-repeat "}
           style={{
-            backgroundImage: `url(https://raw.githubusercontent.com/Kalfreight-In/Kalgroup/main/src/assets/Images/contactimage.png)`, 
+            backgroundImage: `url(https://github.com/Kalfreight-In/Kalgroup/blob/5f2581aefe07eade3b3d02d1369ce98289a12651/src/assets/Images/kalcontactimage.png)`, 
           }}
         >
           {/* <img
           className="w-full"
           src="https://raw.githubusercontent.com/kalfreight-in/kalgroup/e6c3841952235a1866804c85e994f1d5c540c214/src/assets/Images/rednewtruck.svg"
           alt="" 
-        /> */}   
-        </div> 
+        /> */}
+        </div>
       </div>
     </div>
     // </div>
