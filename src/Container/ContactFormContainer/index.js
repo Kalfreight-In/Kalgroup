b5bfed9cfe0aea8fa458ea3ea00e4a8e18764ac6
Kd <umarkhurshid3@gmail.com>
2022-06-23 05:15:07 +0530

    contact comp update

--- src/Container/ContactFormContainer/index.js
+++ src/Container/ContactFormContainer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Contactform from '../../Components/ContactForm/';
+import { isMobile } from 'react-device-detect';
 
 export const ContactFormContainer = () => {
   return (
@@ -28,11 +29,13 @@ export const ContactFormContainer = () => {
           width="300"
         />
       </div> */}
+      
       <div className="flex-1 ">
         <div
           className={
             // 'md:h-5/6 h-mobileMainImageHeight w-4/6 bg-cover bg-no-repeat bg-center mt-16 ml-36' ,
-            'lg:h-5/6 h-mobileMainImageHeight xl:w-4/5 w-full 2 bg-contain lg:bg-cover  xl:contain bg-no-repeat bg-center xl:mt-16 mt-2 2xl:ml-36  '
+            'lg:h-5/6 md:h-mobileMainImageHeight xl:w-4/5 w-full 2 bg-contain lg:bg-cover  xl:contain bg-no-repeat bg-center xl:mt-16 mt-2 2xl:ml-36 '
+         
           }
           style={{
             backgroundImage: `url(https://raw.githubusercontent.com/Kalfreight-In/Kalgroup/main/src/assets/Images/contactimagenew.png)`,
