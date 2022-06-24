eeed3345c1ba005612199e2ccd1313523a53be83
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-25 04:38:00 +0530

    changes

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -17,6 +17,9 @@ import {
 // import { animateScroll as scroll, Link } from 'react-scroll';
 const Sidebar = ({ isOpen, toggle }) => {
   const [Services, setServices] = useState(false);
+  const [NewsRoom, setNewsRoom] = useState(false);
+  const [AboutUs, setAboutUs] = useState(false);
+  const [BusinessVerticles, setBusinessVerticles] = useState(false);
   // const [Logistics, setLogistics] = useState(false);
   // const [Partz, setPartz] = useState(false);
   // const [Tires, setTires] = useState(false);
@@ -40,18 +43,109 @@ const Sidebar = ({ isOpen, toggle }) => {
         <SidebarMenu>
           <SidebarLinkR to="/">HOME</SidebarLinkR>
 
-          <SidebarLinkR to="/kalgroup/tires">Business Verticals</SidebarLinkR>
-          <SidebarLinkR to="/kalgroup/tires">About Us</SidebarLinkR>
-          <SidebarLinkR to="/kalgroup/tires"> Newsroom</SidebarLinkR>
+          {/* <SidebarLinkR onClick={toggle} to="/kalgroup/tires">
+           
+          </SidebarLinkR>
+          <SidebarLinkR onClick={toggle} to="/kalgroup/tires">
+            About Us
+          </SidebarLinkR>
+          <SidebarLinkR onClick={toggle} to="/kalgroup/tires">
+            {' '}
+            Newsroom
+          </SidebarLinkR> */}
+          <Divlink
+            onClick={() => {
+              setBusinessVerticles(!BusinessVerticles);
+              setAboutUs(false);
+              setNewsRoom(false);
+              setServices(false);
+            }}
+          >
+            Business Verticals
+          </Divlink>
+          {BusinessVerticles
+            ? NavbarData[0].navItems.map((data) => (
+                <a href={data.url.url}>
+                  <div className=" text-black  justify-center items-center text-center p-1">
+                    {data.navItem}
+                    {/* {data.navItems
+                ? data.navItems.map((data) => (
+                    <div className=" text-Heading  justify-center text-center items-center">
+                      {data.navItem}
+                    </div>
+                  ))
+                : null} */}
+                  </div>
+                </a>
+              ))
+            : null}
+          <Divlink
+            onClick={() => {
+              setAboutUs(!AboutUs);
+              setBusinessVerticles(false);
+
+              setNewsRoom(false);
+              setServices(false);
+            }}
+          >
+            About Us
+          </Divlink>
+          {AboutUs
+            ? NavbarData[1].navItems.map((data) => (
+                <a href={data.url.url}>
+                  <div className=" text-black  justify-center items-center text-center p-1">
+                    {data.navItem}
+                    {/* {data.navItems
+                ? data.navItems.map((data) => (
+                    <div className=" text-Heading  justify-center text-center items-center">
+                      {data.navItem}
+                    </div>
+                  ))
+                : null} */}
+                  </div>
+                </a>
+              ))
+            : null}
+          <Divlink
+            onClick={() => {
+              setNewsRoom(!NewsRoom);
+              setBusinessVerticles(false);
+              setAboutUs(false);
+
+              setServices(false);
+            }}
+          >
+            News Room
+          </Divlink>
+          {NewsRoom
+            ? NavbarData[2].navItems.map((data) => (
+                <a href={data.url.url}>
+                  <div className=" text-black  justify-center items-center text-center p-1">
+                    {data.navItem}
+                    {/* {data.navItems
+                ? data.navItems.map((data) => (
+                    <div className=" text-Heading  justify-center text-center items-center">
+                      {data.navItem}
+                    </div>
+                  ))
+                : null} */}
+                  </div>
+                </a>
+              ))
+            : null}
           <Divlink
             onClick={() => {
               setServices(!Services);
+              setBusinessVerticles(false);
+              setAboutUs(false);
+              setNewsRoom(false);
             }}
           >
             Services
           </Divlink>
           {Services
             ? NavbarData[3].navItems.map((data) => (
+                // <a href={data.url.url}>
                 <div className=" text-black  justify-center items-center text-center p-1">
                   {data.navItem}
                   {/* {data.navItems
@@ -62,13 +156,38 @@ const Sidebar = ({ isOpen, toggle }) => {
                   ))
                 : null} */}
                 </div>
+                // </a>
               ))
             : null}
-          <SidebarLinkR to="/kalgroup/tires">Careers</SidebarLinkR>
-          <SidebarLinkR to="/kalgroup/tires">Contact Us</SidebarLinkR>
-          <SidebarLinkR to="/Location">
+          <SidebarLinkR to="ContactSection">
             <Link
-              to="Location"
+              to="ContactSection"
+              onClick={toggle}
+              smooth={true}
+              duration={1000}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              Careers
+            </Link>
+          </SidebarLinkR>
+          <SidebarLinkR to="ContactSection">
+            <Link
+              to="ContactSection"
+              onClick={toggle}
+              smooth={true}
+              duration={1000}
+              spy={true}
+              exact={true}
+              offset={-80}
+            >
+              Contact Us
+            </Link>
+          </SidebarLinkR>
+          <SidebarLinkR onClick={toggle} to="mainmapcontainer">
+            <Link
+              to="mainmapcontainer"
               onClick={toggle}
               smooth={true}
               duration={1000}
