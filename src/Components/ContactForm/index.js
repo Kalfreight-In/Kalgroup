5d04f9308f5a29c561d1968daf60d8a48bca16e7
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-25 01:26:57 +0530

    chnges merge

--- src/Components/ContactForm/index.js
+++ src/Components/ContactForm/index.js
@@ -4,7 +4,6 @@ import Sparkles from '../../animation/Sparkel';
 import { motion } from 'framer-motion';
 import { isDesktop } from 'react-device-detect';
 
-
 const Contactform = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -89,7 +88,7 @@ const Contactform = () => {
               <motion.div
                 animate={{ scale: [0.8, 1] }}
                 transition={{
-                  ease: "linear",
+                  ease: 'linear',
                   duration: 0.9,
                   repeat: Infinity,
                 }}
@@ -115,18 +114,16 @@ const Contactform = () => {
                 Contact Us
               </h1>
             </div>
-            <div className='2xl:hidden'> 
+            <div className="2xl:hidden">
               <p className="text-neautralform  font-Poppins md:text-xl text-xs font-normal  my-1 mb-3 ">
                 Got a question? Or request a quote? Contact us directly or fill
-                up the form and we will get back to
-                you promptly.
+                up the form and we will get back to you promptly.
               </p>
             </div>
-            <div className='hidden 2xl:block'> 
+            <div className="hidden 2xl:block">
               <p className="text-neautralform  font-Poppins md:text-xl text-xs font-normal  my-1 mb-3 ">
                 Got a question? Or request a quote? Contact us directly or fill
-                up the form <br/> and we will get back to
-                you promptly.
+                up the form <br /> and we will get back to you promptly.
               </p>
             </div>
           </div>
--- src/Components/NewsShowcase/index.js
+++ src/Components/NewsShowcase/index.js
@@ -21,7 +21,10 @@ const NewsShowcase = (data) => {
     <>
       <div class="flex bg-Lightblue   sm:m-4 w-full md:h-96 xl:h-96 2xl:h-30">
         <div class="flex  flex-row-reverse ">
-          <div class="sm:block hidden 2xl:ml-12 xl:ml-8 md:ml-5  lg:ml-4    mb-10 mt-4 2xl:pt-4 pt-0 mx-6 flex-1 pb-1 ">
+          <div class="sm:block hidden 2xl:ml-12 xl:ml-8 md:ml-5  lg:ml-4    mb-10 mt-10 2xl:pt-4 pt-0 mx-6 flex-1 pb-1 ">
+            <h1 class="text-white text-sm sm:text- 2xl:text-xl font-heading lg:text-2xl  px-0 mb-4">
+              {data.data.date}
+            </h1>
             <h1 className="text-white text-lg  2xl:text-xl  font-heading   px-0 mb-4">
               {data.data.heading}
             </h1>
@@ -78,11 +81,11 @@ const NewsShowcase = (data) => {
               ) : null}
             </div>
           </div>
-          <div className="  sm:flex-1 flex w-full ">
+          <div className="   flex w-full " style={{ flex: '0  3  60%' }}>
             <div
-              className={`md:h-full h-mobileMainImageHeight w-full bg-cover bg-no-repeat ${
-                isDesktop ? 'bg-left' : 'bg-center'
-              }`}
+              className={
+                'md:h-full h-mobileMainImageHeight w-full bg-cover bg-no-repeat bg-center'
+              }
               style={{
                 backgroundImage: `url(${imageurl})`,
               }}
